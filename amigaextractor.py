afd320bbf4a786afe13da278e25b53493bfb72aa
jbl007 <jbl007@gmail.com>
2015-09-20 20:46:13 +0200

    split extract function, continue silently if overwrite == false, minor fixes

--- amigaextractor.py
+++ amigaextractor.py
@@ -2,58 +2,70 @@
 
 from lhafile import LhaFile
 import os.path
-import pdb
 
 
 class LhaExtractor(LhaFile):
 
     def __init__(self, lha_file_name, *args, **kwargs):
         super().__init__(lha_file_name, *args, **kwargs)
-        self.total_file_size = self.total_compress_size = 0
-        for file in self.filelist:
-            self.total_file_size += file.file_size
-            self.total_compress_size += file.compress_size
+        self.total_file_size = sum(
+            [ f.file_size for f in self.filelist ])
+        self.total_compress_size = sum(
+            [ f.compress_size for f in self.filelist ])
         self.total_ratio = self.total_compress_size * 100 / (
             self.total_file_size if self.total_file_size else 1)
-        self.total_ratio = round(self.total_ratio, 2)
+
+    def __write_metadata(self, file_to_extract, target_file, force):
+        filenote = self.NameToInfo[file_to_extract].comment or ''
+        protection_flags = self.NameToInfo[
+            file_to_extract].flag_bits or '----rwed'
+        if force is not True:
+            if filenote == '' and protection_flags == '----rwed':
+                return
+        file_date = self.NameToInfo[
+            file_to_extract].date_time.strftime('%F %T.00')
+        uaem_string = '{0} {1} {2}\n'.format(
+            protection_flags, file_date, filenote)
+        with open('{0}.uaem'.format(target_file), 'wt') as uaem_file:
+            uaem_file.write(uaem_string)
 
     def list_files(self, verbose=False):
         if not verbose:
-            for file in self.namelist():
-                print(file)
+            for item in self.namelist():
+                print(item)
             return
         num_files = len(self.filelist)
         print(' PACKED  SIZE   RATIO  METHOD CRC       STAMP           PERM        NAME')
         print('------- ------- ------ ---------- ------------------- -------- --------------')
-        for file in self.filelist:
-            ratio = file.compress_size * 100 / (
-                file.file_size if file.file_size else 1)
-            print(str(file.compress_size).rjust(7), end=' ')
-            print(str(file.file_size).rjust(7), end=' ')
+        for item in self.filelist:
+            ratio = item.compress_size * 100 / (
+                item.file_size if item.file_size else 1)
+            print(str(item.compress_size).rjust(7), end=' ')
+            print(str(item.file_size).rjust(7), end=' ')
             print('{0:.2f}'.format(ratio).rjust(5), end='% ')
-            print(file.compress_type.decode(), end=' ')
-            print(hex(file.CRC)[2:].zfill(4), end=' ')
-            print(file.date_time.strftime('%F %T'), end=' ')
-            print(file.flag_bits, end=' ')
-            print(file.filename)
-            if file.comment:
-                print(' : {0}'.format(file.comment))
+            print(item.compress_type.decode(), end=' ')
+            print(hex(item.CRC)[2:].zfill(4), end=' ')
+            print(item.date_time.strftime('%F %T'), end=' ')
+            print(item.flag_bits, end=' ')
+            print(item.filename)
+            if item.comment:
+                print(' : {0}'.format(item.comment))
         print('------- ------- ------ ---------- ------------------- -------- --------------')
         print(str(self.total_compress_size).rjust(7), end=' ')
         print(str(self.total_file_size).rjust(7), end=' ')
         print('{0:.2f}'.format(self.total_ratio).rjust(5), end='% ')
-        print(str(num_files).rjust(40), end=' file(s)\n')
-        print(file.create_system)
+        print('Created on: {0}'.format(item.create_system).center(30), end=' ')
+        print(str(num_files).rjust(10), end=' file(s)\n')
 
     def extract(self, filename=None, uaem='auto',
                 dest='.', use_paths=True, overwrite=False, verbose=False):
         if filename and filename not in self.namelist():
-            raise FileNotFoundError('File not found in archive')
+            return False, 'File not found in archive'
         if uaem not in ('auto', 'always', 'never'):
             raise ValueError('uaem must be auto, always or never')
         target_dir = os.path.realpath(dest)
         if not os.path.isdir(target_dir):
-            raise FileNotFoundError('Target directory does not exist.')
+            return False, 'Target directory does not exist.'
         if not filename:
             files_to_extract = self.namelist()
         else:
@@ -67,39 +79,20 @@ class LhaExtractor(LhaFile):
                     target_dir, os.sep.join(file_to_extract.split('\\')))
                 os.makedirs(os.path.dirname(target_file), exist_ok=True)
             if verbose:
-                print(target_file, end=' ')
+                print(target_file)
             if not overwrite and os.path.isfile(target_file):
-                #raise FileExistsError('File already exists')
-                #continue
-                return False, '{0} already exists.'.format(target_file)
+                continue
             try:
                 data = self.read(file_to_extract)
             except Exception as e:
                 return False, e.args[0]
             with open(target_file, 'wb') as output_file:
                 output_file.write(data)
-            filenote = self.NameToInfo[file_to_extract].comment or ''
-            protection_flags = self.NameToInfo[
-                file_to_extract].flag_bits or '----rwed'
-            write_flags = False
+
             if uaem == 'always':
-                write_flags = True
+                self.__write_metadata(file_to_extract, target_file, force=True)
             elif uaem == 'auto':
-                if filenote:
-                    write_flags = True
-                if protection_flags != '----rwed':
-                    write_flags = True
-            if write_flags:
-                file_date = self.NameToInfo[
-                    file_to_extract].date_time.strftime('%F %T.00')
-                uaem_string = '{0} {1} {2}\n'.format(
-                    protection_flags, file_date, filenote)
-                if verbose:
-                    print(uaem_string)
-                with open('{0}.uaem'.format(target_file), 'wt') as uaem_file:
-                    uaem_file.write(uaem_string)
-            if verbose:
-                print()
+                self.__write_metadata(file_to_extract, target_file, force=False)
         return True, ''
 
     def testlha(self):
@@ -116,7 +109,7 @@ class LhaExtractor(LhaFile):
         return is_ok
 
     def printdir(self):
-        LhaExtractor.list_files(self, verbose=False)
+        self.list_files(verbose=False)
 
 
 if __name__ == '__main__':
